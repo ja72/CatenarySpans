6775587049e7e2b955b1e396cb230ef8f8ba4871
John Alexiou <jalexiou@hotmail.com>
2020-02-15 12:13:18 -0500

    Remove unused code

--- JA-CatenarySpans/Program.cs
+++ JA-CatenarySpans/Program.cs
@@ -15,32 +15,9 @@ namespace JA
         [STAThread]
         static void Main()
         {
-            //GenLicense();
-            //SagTensTests.TestCatSol();
-            //SagTensTests.TestSagTension();
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new JA.CatenarySpans.CatenaryForm());
-        }
-
-        static void GenLicense()
-        {
-            Guid guid;
-            string strGuid = null;
-            do
-            {
-                guid = Guid.NewGuid();
-                strGuid = guid.ToString();
-                //byte[] guidBytes = guid.ToByteArray();
-                //byte[] userBytes = Encoding.UTF7.GetBytes(Environment.UserName);
-                //var sha1 = guidBytes.Concat(userBytes).HashSha1();
-                var sha1 = (strGuid + Environment.UserName).HashSha1();
-                strGuid += "-" + sha1.ToString("x8");
-
-            } while (strGuid.Length!=53);
-
-            Debug.WriteLine(strGuid);
         }
 
     }
